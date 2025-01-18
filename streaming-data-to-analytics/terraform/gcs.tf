2ebf772a0c8b9a65705ea705d959fc712a326aad
Mao Kano <maoye@ad.corp.google.com>
2025-01-19 00:23:01 +0900

    refs # add code to zip cloud function source code

--- streaming-data-to-analytics/terraform/gcs.tf
+++ streaming-data-to-analytics/terraform/gcs.tf
@@ -7,3 +7,10 @@ resource "google_storage_bucket" "cloudfunctions_tmp_bucket" {
 
  uniform_bucket_level_access = true
 }
+
+# Upload Cloud Functions Source Zip to Cloud
+resource "google_storage_bucket_object" "archive" {
+  name   = var.rcs_metrics_saver_cf_zip_name
+  bucket = google_storage_bucket.cloudfunctions_tmp_bucket.name
+  source = "./tmp/"+var.rcs_metrics_saver_cf_zip_name # Path to the zip file containing your Cloud Function code
+}
