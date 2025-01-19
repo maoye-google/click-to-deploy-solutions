d33c8329e759919c155dfd03ea2af958c33938d1
Mao Kano <maoye@ad.corp.google.com>
2025-01-19 21:57:58 +0900

    refs # update IAM setting to grant permission for pubsub subscription change

--- streaming-data-to-analytics/terraform/iam.tf
+++ streaming-data-to-analytics/terraform/iam.tf
@@ -4,24 +4,10 @@ resource "google_service_account" "ingest_api" {
   create_ignore_already_exists = true
 }
 
-resource "google_project_iam_member" "publisher" {
+resource "google_project_iam_member" "token_creator" {
   project = var.project_id
-  role    = "roles/pubsub.publisher"
+  role    = "roles/iam.serviceAccountTokenCreator"
   member  = "serviceAccount:${google_service_account.ingest_api.email}"
-  
-  depends_on = [
-    google_service_account.ingest_api
-  ]
-}
-
-resource "google_project_iam_member" "subscriber" {
-  project = var.project_id
-  role    = "roles/pubsub.subscriber"
-  member  = "serviceAccount:${google_service_account.ingest_api.email}"
-
-  depends_on = [
-    google_service_account.ingest_api
-  ]
 }
 
 resource "google_project_iam_member" "cloudrun_invoker" {
@@ -36,20 +22,15 @@ resource "google_project_iam_member" "cloudfunction_invoker" {
   member  = "serviceAccount:${google_service_account.ingest_api.email}"
 }
 
-resource "google_project_iam_member" "token_creator" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountTokenCreator"
-  member  = "serviceAccount:${google_service_account.ingest_api.email}"
-}
 
-# Grant the Cloud Run's service account permissions to write to Cloud Monitoring
+# Grant the permissions to write to Cloud Monitoring
 resource "google_project_iam_member" "cloud_monitoring_writer" {
   project = var.project_id
   role    = "roles/monitoring.editor"
   member  = "serviceAccount:${google_service_account.ingest_api.email}"
 }
 
-# Grant the Cloud PubSub service account permissions to write to BigQuery
+# Grant the permissions to write to BigQuery
 resource "google_project_iam_member" "pubsub_bqEditor" {
   project = var.project_id
   role    = "roles/bigquery.dataEditor"
@@ -57,9 +38,21 @@ resource "google_project_iam_member" "pubsub_bqEditor" {
   member  = "serviceAccount:${google_service_account.ingest_api.email}"
 }
 
+# Grant the Permission to get Bigquery metadata
 resource "google_project_iam_member" "pubsub_bqMetadata" {
   project = var.project_id
   role    = "roles/bigquery.metadataViewer"
   # member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
   member  = "serviceAccount:${google_service_account.ingest_api.email}"
+}
+
+# Grant the Permission to modify pubsub subscriptions, and consumer messages
+resource "google_project_iam_member" "pubsub_editor" {
+  project = var.project_id
+  role    = "roles/pubsub.editor"
+  member  = "serviceAccount:${google_service_account.ingest_api.email}"
+  
+  depends_on = [
+    google_service_account.ingest_api
+  ]
 }
